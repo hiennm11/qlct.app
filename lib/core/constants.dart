10e51ebfcd9558ed7dc9c0b737c311439c5cfc32
Minh Hiền <minhhiena712@gmail.com>
2026-06-12 22:45:03 +0700

    chore(release): sync about version

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -4,7 +4,7 @@ class AppConstants {
 
   // App info
   static const String appName = 'Quản Lý Chi Tiêu';
-  static const String appVersion = '1.2.0';
+  static const String appVersion = '1.5.0';
 
   // Storage keys
   static const String transactionsKey = 'transactions';
